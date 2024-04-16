6befe40ae424fb143fa0601d9802c4a45f7e973e
João Vítor Braga da Silva Alves <53454378+joaovitor1704@users.noreply.github.com>
2024-04-16 20:38:06 +0000

    Inicial

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
+
 #define CROW_MAIN
 #define CROW_STATIC_DIR "../public"
 
 #include "crow_all.h"
 #include "json.hpp"
 #include <random>
+#include <thread>
+#include <chrono>
+#include <iostream>
 
 static const uint32_t NUM_ROWS = 15;
 
@@ -22,6 +26,10 @@ const double HERBIVORE_MOVE_PROBABILITY = 0.7;
 const double HERBIVORE_EAT_PROBABILITY = 0.9;
 const double CARNIVORE_MOVE_PROBABILITY = 0.5;
 const double CARNIVORE_EAT_PROBABILITY = 1.0;
+
+
+uint32_t linha = 0;
+uint32_t coluna = 0;
 
 // Type definitions
 enum entity_type_t
@@ -65,6 +73,25 @@ namespace nlohmann
 // Grid that contains the entities
 static std::vector<std::vector<entity_t>> entity_grid;
 
+void entity_planta(){
+
+    if(entity_grid[linha][coluna].age >= PLANT_MAXIMUM_AGE){
+        entity_grid[linha][coluna] = {empty, 0, 0};
+    } 
+    else{
+        entity_grid[linha][coluna].age += 1;
+        if(rand()/double(RAND_MAX) < PLANT_REPRODUCTION_PROBABILITY){
+            if(linha + 1 < NUM_ROWS && coluna + 1 < NUM_ROWS){
+                if(entity_grid[linha+1][coluna].type == empty){
+                    entity_grid[linha+1][coluna] = {plant, 0, 0};
+                }
+            }               
+        }
+    }
+}
+
+
+
 int main()
 {
     crow::SimpleApp app;
@@ -98,6 +125,27 @@ int main()
         
         // Create the entities
         // <YOUR CODE HERE>
+        int num_plants = (uint32_t)request_body["plants"];
+        int num_herbivores = (uint32_t)request_body["herbivores"];
+        int num_carnivores = (uint32_t)request_body["carnivores"];
+
+        for (int i = 0; i < num_plants; i++)
+        {
+            pos_t pos = {rand() % NUM_ROWS, rand() % NUM_ROWS};
+            entity_grid[pos.i][pos.j] = {plant, 0, 0};
+        }
+
+        for (int i = 0; i < num_herbivores; i++)
+        {
+            pos_t pos = {rand() % NUM_ROWS, rand() % NUM_ROWS};
+            entity_grid[pos.i][pos.j] = {herbivore, 100, 0};
+        }
+
+        for (int i = 0; i < num_carnivores; i++)
+        {
+            pos_t pos = {rand() % NUM_ROWS, rand() % NUM_ROWS};
+            entity_grid[pos.i][pos.j] = {carnivore, 0, 0};
+        }
 
         // Return the JSON representation of the entity grid
         nlohmann::json json_grid = entity_grid; 
@@ -113,6 +161,18 @@ int main()
         
         // <YOUR CODE HERE>
         
+        for (linha = 0; linha < NUM_ROWS; linha++)
+        {
+            for (coluna = 0; coluna < NUM_ROWS; coluna++)
+            {
+                if(entity_grid[linha][coluna].type == plant){
+                    entity_planta();
+
+                }
+            }
+        }
+        
+
         // Return the JSON representation of the entity grid
         nlohmann::json json_grid = entity_grid; 
         return json_grid.dump(); });
